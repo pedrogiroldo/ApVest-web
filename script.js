fc716cb0beb897bede212b379fa66695461d8872
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-26 17:08:04 -0300

    Função que valida os alunos aprovados na lista

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-const aprovadosFilePath = document.getElementById('lista-aprovados');
-const alunosFilePath = document.getElementById('lista-alunos');
+function encontrarAlunosAprovados(alunosData, aprovadosData) {
 
-function encontrarAlunosAprovados(alunosFilePath, aprovadosFilePath) {
-    const fs = require('fs');
-    const alunos = fs.readFileSync(alunosFilePath, 'utf-8').split('\n');
-    const aprovados = fs.readFileSync(aprovadosFilePath, 'utf-8').split('\n');
-    
-    const alunosSet = new Set(alunos);
-    const aprovadosSet = new Set(aprovados);
-    
-    const alunosAprovados = [];
-    
-    alunosSet.forEach((aluno) => {
-      if (aprovadosSet.has(aluno)) {
-        alunosAprovados.push(aluno);
-      }
-    });
-    
-    console.log(alunosAprovados)
-    return alunosAprovados;
-  }
+  //regex para tirar os \r
+  const alunos = alunosData.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split('\n');
+  const aprovados = aprovadosData.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split('\n');
 
-  const client_button = document
-  .getElementById("botao")
-  .addEventListener("click", encontrarAlunosAprovados);
+
+  const alunosSet = new Set(alunos);
+  const aprovadosSet = new Set(aprovados);
+
+  const alunosAprovados = [];
+
+  alunosSet.forEach((aluno) => {
+    if (aprovadosSet.has(aluno)) {
+      alunosAprovados.push(aluno);
+    }
+  });
+
+  return alunosAprovados;
+}
+
+const alunosFileInput = document.getElementById('lista-alunos');
+const aprovadosFileInput = document.getElementById('lista-aprovados');
+
+const client_button = document
+  .getElementById('input_client_button')
+  .addEventListener('click', () => {
+    const alunosFile = alunosFileInput.files[0];
+    const aprovadosFile = aprovadosFileInput.files[0];
+
+    const reader = new FileReader();
+    reader.readAsText(alunosFile);
+    reader.onload = () => {
+      const alunosData = reader.result;
+      const reader2 = new FileReader();
+      reader2.readAsText(aprovadosFile);
+      reader2.onload = () => {
+        const aprovadosData = reader2.result;
+        const alunosAprovados = encontrarAlunosAprovados(alunosData, aprovadosData);
+        console.log(alunosAprovados);
+      };
+    };
+  });
+
