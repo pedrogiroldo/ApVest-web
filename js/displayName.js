1d66431d866601f289f8a8a742e45c491b3d3c20
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-27 14:36:04 -0300

    add green tick

--- js/displayName.js
+++ js/displayName.js
@@ -1,14 +1,22 @@
 const listaAlunosInput = document.getElementById('lista-alunos');
-  const listaAlunosLabel = document.querySelector('label[for="lista-alunos"]');
-  const listaAprovadosInput = document.getElementById('lista-aprovados');
-  const listaAprovadosLabel = document.querySelector('label[for="lista-aprovados"]');
+const listaAlunosLabel = document.querySelector('label[for="lista-alunos"]');
+const listaAprovadosInput = document.getElementById('lista-aprovados');
+const listaAprovadosLabel = document.querySelector('label[for="lista-aprovados"]');
+const listaAlunosTick = document.getElementById('greenTickInput1')
+const listaAprovadosTick = document.getElementById('greenTickInput2')
+
+listaAprovadosTick.style.display = "none";
+listaAlunosTick.style.display = "none";
 
   // Adiciona um evento para quando o arquivo for selecionado
   listaAlunosInput.addEventListener('change', function() {
     // Exibe o nome do arquivo selecionado
     listaAlunosLabel.innerHTML = this.files[0].name;
+    listaAlunosTick.style.display = '';
+    
   });
-
+  
   listaAprovadosInput.addEventListener('change', function() {
     listaAprovadosLabel.innerHTML = this.files[0].name;
+    listaAprovadosTick.style.display = '';
   });
