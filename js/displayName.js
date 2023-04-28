dd5878bd8092eea55f4fbe2093f0acdaddd23809
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-28 19:18:33 -0300

    first chenges on red things

--- js/displayName.js
+++ js/displayName.js
@@ -5,18 +5,25 @@ const listaAprovadosLabel = document.querySelector('label[for="lista-aprovados"]
 const listaAlunosTick = document.getElementById('greenTickInput1')
 const listaAprovadosTick = document.getElementById('greenTickInput2')
 
+// const listaAlunosRedTick = document.getElementById('redTickInput1')
+// const listaAprovadosRedTick = document.getElementById('redTickInput2')
+
 listaAprovadosTick.style.display = "none";
 listaAlunosTick.style.display = "none";
+
+// listaAprovadosRedTick.style.display = "none";
+// listaAlunosRedTick.style.display = "none";
 
   // Adiciona um evento para quando o arquivo for selecionado
   listaAlunosInput.addEventListener('change', function() {
     // Exibe o nome do arquivo selecionado
     listaAlunosLabel.innerHTML = this.files[0].name;
     listaAlunosTick.style.display = '';
+    listaAlunosRedTick.style.display = 'none';
     
   });
   
   listaAprovadosInput.addEventListener('change', function() {
     listaAprovadosLabel.innerHTML = this.files[0].name;
-    listaAprovadosTick.style.display = '';
+    listaAprovadosTick.style.display = 'none';
   });
--- js/shakebutton.js
+++ js/shakebutton.js
@@ -1,5 +1,11 @@
 const button = document.getElementById('input_client_button');
 
+const listaAlunosRedTick = document.getElementById('redTickInput1')
+const listaAprovadosRedTick = document.getElementById('redTickInput2')
+
+listaAprovadosRedTick.style.display = "none";
+listaAlunosRedTick.style.display = "none";
+
 button.addEventListener('click', () => {
   const universityInput = document.getElementById('nomeVest');
   // const universityInputCSS = document.querySelector('inputVest');
@@ -12,15 +18,19 @@ button.addEventListener('click', () => {
     // adiciona a classe 'shake' aos inputs vazios
     if (!universityInput.value) {
       universityInput.classList.add('shake');
+      universityInput.style.borderColor = 'red';
+      universityInput.style.boxShadow = '0 0 5px red';
       // universityInput.classList.add.boxShadow(0, 0, 5, rgba(225, 0, 0))
       //alert('Por favor, preencha o campo "Nome da Universidade".');
     }
     if (!alunosInput.value) {
       alunosInput.classList.add('shake');
+      listaAlunosRedTick.style.display = '';
       //alert('Por favor, preencha o campo "Lista de Alunos".');
     }
     if (!aprovadosInput.value) {
       aprovadosInput.classList.add('shake');
+      listaAprovadosRedTick.style.display = '';
       //alert('Por favor, preencha o campo "Lista de Aprovados".');
     }
 
