96154a04d6031dcf7401173f0d5c37d85f3bece9
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-29 21:06:07 -0300

    style bugs solved

--- js/shakebutton.js
+++ js/shakebutton.js
@@ -10,11 +10,10 @@ listaAlunosRedTick.style.display = "none";
 
 button.addEventListener('click', () => {
   const universityInput = document.getElementById('nomeVest');
-  // const universityInputCSS = document.querySelector('inputVest');
-  // const alunosInput = document.getElementById('lista-alunos');
-  const alunosInput = document.querySelector('.input1');
-  // const aprovadosInput = document.getElementById('lista-aprovados');
-  const aprovadosInput = document.querySelector('.input2');
+  const alunosInput = document.getElementById('lista-alunos');
+  const aprovadosInput = document.getElementById('lista-aprovados');
+  const alunosInputCSS = document.querySelector('.input1');
+  const aprovadosInputCSS = document.querySelector('.input2');
 
   if (!universityInput.value || !alunosInput.value || !aprovadosInput.value) {
     // adiciona a classe 'shake' aos inputs vazios
@@ -25,28 +24,37 @@ button.addEventListener('click', () => {
       // universityInput.classList.add.boxShadow(0, 0, 5, rgba(225, 0, 0))
       //alert('Por favor, preencha o campo "Nome da Universidade".');
     }
+    console.log(alunosInput)
     if (!alunosInput.value) {
-      alunosInput.classList.add('shake');
+      alunosInputCSS.classList.add('shake');
       listaAlunosRedTick.style.display = '';
-      buttonCSS.style.borderColor = 'red';
-      buttonCSS.style.boxShadow = '0 0 5px red';
       //alert('Por favor, preencha o campo "Lista de Alunos".');
     }
+    console.log(aprovadosInput)
     if (!aprovadosInput.value) {
-      aprovadosInput.classList.add('shake');
+      aprovadosInputCSS.classList.add('shake');
       listaAprovadosRedTick.style.display = '';
-      buttonCSS.style.boxShadow = '0 0 5px red';
-      buttonCSS.style.borderColor = 'red';
       //alert('Por favor, preencha o campo "Lista de Aprovados".');
     }
 
     // remove a classe 'shake' após 500ms
     setTimeout(() => {
       universityInput.classList.remove('shake');
-      alunosInput.classList.remove('shake');
-      aprovadosInput.classList.remove('shake');
+      alunosInputCSS.classList.remove('shake');
+      aprovadosInputCSS.classList.remove('shake');
     }, 500);
 
     return false;
   }
 });
+
+
+const universityInput = document.getElementById('nomeVest');
+
+universityInput.onchange = ()=>{
+  if(universityInput.value){
+    universityInput.style.borderColor = '';
+    universityInput.style.boxShadow = '';
+  }
+
+}
